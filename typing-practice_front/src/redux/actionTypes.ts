f345fcc03930a98fe665081adfe448ea404bc5fc
ivanbaranov182 <ivanbaranov182@gmail.com>
2021-05-06 18:40:32 +0300

    changes

--- typing-practice_front/src/redux/actionTypes.ts
+++
@@ -1,6 +0,0 @@
-export const LOGIN_USER = 'LOGIN_USER';
-export const REGISTER_USER = 'REGISTER_USER';
-export const FORGOT_PASSWORD = 'FORGOT_PASSWORD';
-export const LOGOUT_USER = 'LOGOUT_USER';
-
-export const GET_USER_INFO = 'GET_USER_INFO';
